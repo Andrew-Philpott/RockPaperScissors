399a0adc3054f6519025be298d6da27ce9ba31ba
Andrew Philpott <andrewphilpott92@gmail.com>
2020-03-04 08:37:16 -0800

    Add boilerplate code.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using RockPaperScissors.Models;
+using System.Collections.Generic;
+
+namespace RockPaperScissors
+{
+  public class Program
+  {
+
+    public static void Main()
+    {
+
+    }
+  }
+}
